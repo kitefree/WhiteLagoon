9e89cd323d46cc72566fb12ed4071aaafd93f239
kite <kitefree@gmail.com>
2023-11-13 16:50:03 +0800

    加入專案檔案。

---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public Double Price { get; set; }
+        public int Sqft { get; set; }
+        public int Qccupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
