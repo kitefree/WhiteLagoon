d5c77242bcc54c91b8055ef622f23121b87b0743
kite <kitefree@gmail.com>
2023-11-14 16:41:58 +0800

    add 資料庫連線設定、注入資料庫連線、建立seed villa data、顯示villa data to view、code first

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -9,11 +9,11 @@ namespace WhiteLagoon.Domain.Entities
     public class Villa
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string? Description { get; set; }
         public Double Price { get; set; }
         public int Sqft { get; set; }
-        public int Qccupancy { get; set; }
+        public int Occupancy { get; set; }
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
---
+++ WhiteLagoon.Infrastructure/Migrations/20231114030035_修改Qccupancy欄位名稱，改為Occupancy.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class 修改Qccupancy欄位名稱改為Occupancy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Qccupancy",
+                table: "Villas",
+                newName: "Occupancy");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Occupancy",
+                table: "Villas",
+                newName: "Qccupancy");
+        }
+    }
+}
