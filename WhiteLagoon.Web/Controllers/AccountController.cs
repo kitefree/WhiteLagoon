e2ba9a2611f35095c7523ff087d7f590e1727f4b
DESKTOP-I40U13Q\kite <kitefree@gmail.com>
2023-11-20 19:14:14 +0800

    建立帳號Controller、LoginView

---
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Repository;
+using WhiteLagoon.Web.ViewModels;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        
+        public AccountController(IUnitOfWork unitOfWork,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _roleManager = roleManager;                        
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            LoginVM loginVM = new LoginVM
+            {
+                RedirectUrl = returnUrl
+            };
+
+            return View(loginVM);
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
