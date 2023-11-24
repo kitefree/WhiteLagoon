74155495e468b62cd2c2f958b02adee2eeb1cc70
DESKTOP-I40U13Q\kite <kitefree@gmail.com>
2023-11-24 13:57:12 +0800

    移除多餘程式碼

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -27,24 +27,10 @@ namespace WhiteLagoon.Web.Controllers
             return View(homeVM);
         }
 
-        [HttpPost]
-        public IActionResult Index(HomeVM homeVM)
-        {
-            homeVM.VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity");
-
-            foreach (var villa in homeVM.VillaList)
-            {
-                if (villa.Id % 2 == 0)
-                {
-                    villa.IsAvailable = false;
-                }
-            }
-
-            return View(homeVM);
-        }
 
         public IActionResult GetVillasByDate(int nights,DateTime checkInDate)
         {
+            
             var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity");
 
             foreach (var villa in villaList)
